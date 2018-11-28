0f24c324c39030d6b2bc669449c0c9ca3e33febd
lartl <luis.arce22@gmail.com>
2018-11-27 22:31:57 -0400

    Use bootstrap classes

--- src/comps/Product.js
+++ src/comps/Product.js
@@ -2,16 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Product = ({ product, onSave, onChangeName, onChangeCode }) => (
-  <div>
-    Name:
-    <br />
-    <input onChange={onChangeName} value={product.name} />
-    <br />
-    Code:
-    <br />
-    <input onChange={onChangeCode} value={product.code} /> <br />
+  <form>
+    <div className="form-group">
+      <label>Product Name</label>
+      <input
+        type="text"
+        className="form-control"
+        id="productNameId"
+        onChange={onChangeName}
+        value={product.name}
+        placeholder="Enter Product Name"
+      />
+    </div>
+
+    <div className="form-group">
+      <label>Code</label>
+      <input
+        type="text"
+        className="form-control"
+        id="productCodeId"
+        onChange={onChangeCode}
+        value={product.code}
+        placeholder="Enter Product Code"
+      />
+    </div>
     <button onClick={onSave}>Save</button>
-  </div>
+  </form>
 );
 
 Product.propTypes = {
